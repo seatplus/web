14fd129f477a447b9e91a2b57dc15e7dd3f5fb38
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-05-05 14:07:02 +0200

    add webtest

--- tests/WebIndexTest.php
+++ tests/WebIndexTest.php
@@ -12,4 +12,16 @@ class WebIndexTest extends TestCase
 
         $response->assertRedirect('auth/login');
     }
+
+    /** @test */
+    public function redirectsToHomeIfAuthorized()
+    {
+        // Change path.public from Laravel IoC Container to point to proper laravel mix manifest.
+        $this->app->instance('path.public', __DIR__ .'/../src/public');
+
+        $response = $this->actingAs($this->test_user)
+            ->get('/home');
+
+        $response->assertSee('congratulation');
+    }
 }
