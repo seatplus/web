042872893a7e7bd1fff58bd702bba991228d4b3d
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-08-03 12:43:25 +0200

    Optimize header to support font-awesome

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -8,4 +8,6 @@
 
   congratulation this is a Authed page
 
+
+
 @endsection
--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -1,9 +1,9 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
       <a href="index3.html" class="nav-link">Home</a>
@@ -19,7 +19,7 @@
       <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
       <div class="input-group-append">
         <button class="btn btn-navbar" type="submit">
-          <i class="fa fa-search"></i>
+          <i class="fas fa-search"></i>
         </button>
       </div>
     </div>
@@ -30,7 +30,7 @@
     <!-- Messages Dropdown Menu -->
     <li class="nav-item dropdown">
       <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="fa fa-comments-o"></i>
+        <i class="far fa-comments"></i>
         <span class="badge badge-danger navbar-badge">3</span>
       </a>
       <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -41,10 +41,10 @@
             <div class="media-body">
               <h3 class="dropdown-item-title">
                 Brad Diesel
-                <span class="float-right text-sm text-danger"><i class="fa fa-star"></i></span>
+                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
               </h3>
               <p class="text-sm">Call me whenever you can...</p>
-              <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
             </div>
           </div>
           <!-- Message End -->
@@ -57,10 +57,10 @@
             <div class="media-body">
               <h3 class="dropdown-item-title">
                 John Pierce
-                <span class="float-right text-sm text-muted"><i class="fa fa-star"></i></span>
+                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
               </h3>
               <p class="text-sm">I got your message bro</p>
-              <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
             </div>
           </div>
           <!-- Message End -->
@@ -73,10 +73,10 @@
             <div class="media-body">
               <h3 class="dropdown-item-title">
                 Nora Silvester
-                <span class="float-right text-sm text-warning"><i class="fa fa-star"></i></span>
+                <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
               </h3>
               <p class="text-sm">The subject goes here</p>
-              <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
             </div>
           </div>
           <!-- Message End -->
@@ -88,24 +88,24 @@
     <!-- Notifications Dropdown Menu -->
     <li class="nav-item dropdown">
       <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="fa fa-bell-o"></i>
+        <i class="far fa-bell"></i>
         <span class="badge badge-warning navbar-badge">15</span>
       </a>
       <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-header">15 Notifications</span>
+        <span class="dropdown-item dropdown-header">15 Notifications</span>
         <div class="dropdown-divider"></div>
         <a href="#" class="dropdown-item">
-          <i class="fa fa-envelope mr-2"></i> 4 new messages
+          <i class="fas fa-envelope mr-2"></i> 4 new messages
           <span class="float-right text-muted text-sm">3 mins</span>
         </a>
         <div class="dropdown-divider"></div>
         <a href="#" class="dropdown-item">
-          <i class="fa fa-users mr-2"></i> 8 friend requests
+          <i class="fas fa-users mr-2"></i> 8 friend requests
           <span class="float-right text-muted text-sm">12 hours</span>
         </a>
         <div class="dropdown-divider"></div>
         <a href="#" class="dropdown-item">
-          <i class="fa fa-file mr-2"></i> 3 new reports
+          <i class="fas fa-file mr-2"></i> 3 new reports
           <span class="float-right text-muted text-sm">2 days</span>
         </a>
         <div class="dropdown-divider"></div>
@@ -113,8 +113,9 @@
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-                class="fa fa-th-large"></i></a>
+      <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
+        <i class="fas fa-th-large"></i>
+      </a>
     </li>
   </ul>
 </nav>
