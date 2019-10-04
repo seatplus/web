a7b5c5b16f3d0ab0af4ac31f3adbf177c6eb7e3d
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-10-04 16:43:08 +0200

    Introduce AccessCheckerTrait and reduce complexity in middleware

--- src/Http/Middleware/CheckPermissionAndAffiliation.php
+++ src/Http/Middleware/CheckPermissionAndAffiliation.php
@@ -3,7 +3,6 @@
 namespace Seatplus\Web\Http\Middleware;
 
 use Closure;
-use Seatplus\Web\Exceptions\UnauthorizedException;
 
 class CheckPermissionAndAffiliation
 {
@@ -35,43 +34,12 @@ class CheckPermissionAndAffiliation
             ? $permission
             : explode('|', $permission);
 
+        $id_to_check_affiliation = ($id = reset($request->route()->parameters)) ? intval($id) : null;
+
         foreach ($permissions as $permission) {
-            /*
-             * If we think about corporation roles such as director or accountant we should check directPermissions
-             * TODO: create direct permissions to users within CharacterRoles job
-             * TODO: if refreshtoken is revoked remove all direct permissions
-            */
 
-            // start by asserting that the user has the required permission
-            if (app('auth')->user()->hasPermissionTo($permission)) {
-
-                // Check a users roles and only take the ones into account which have a certain permission
-                $users_roles_with_permission = app('auth')->user()->roles->filter(function ($role) use ($permission) {
-                    return $role->hasPermissionTo($permission);
-                });
-
-                // Check if role has an affiliation to the requested resource
-                $users_roles_with_permission_and_affiliation = $users_roles_with_permission->isNotEmpty()
-                    ? $users_roles_with_permission->filter(function ($role) use ($request) {
-
-                        /*
-                         * First we assume that there is always just one route parameter to check:
-                         * either character_id, corporation_id or alliance_id is supplied. As a result
-                         * the first id is taken from the parameters array and converted to an integer.
-                         */
-                        $id_to_check_affiliation = ($id = reset($request->route()->parameters)) ? intval($id) : null;
-
-                        if(is_integer($id_to_check_affiliation))
-                            return $role->isAffiliated($id_to_check_affiliation);
-
-                        return false;
-                    })
-                    : collect();
-
-                // if a role has both permission and affiliation let user see the page
-                if($users_roles_with_permission_and_affiliation->isNotEmpty())
-                    return $next($request);
-            }
+            if((auth()->user()->hasAccessTo($permission, $id_to_check_affiliation)))
+                return $next($request);
         }
 
         return abort(403, 'Missing Permission: ' . $permission);
