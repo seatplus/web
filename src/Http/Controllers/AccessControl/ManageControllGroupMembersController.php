9db809b9063cdfe6826da5bf8d87250f49290980
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-07 19:29:19 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/AccessControl/ManageControllGroupMembersController.php
+++ src/Http/Controllers/AccessControl/ManageControllGroupMembersController.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Web\Http\Controllers\AccessControl;
-
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
@@ -20,9 +42,9 @@ class ManageControllGroupMembersController
                 'id' => $user->id,
                 'name' => $user->main_character->name,
                 'character_id' => $user->main_character->character_id,
-                'characters' => $user->characters->filter(function ($character) use ($user){
+                'characters' => $user->characters->filter(function ($character) use ($user) {
                     return $character->character_id !== $user->main_character->character_id;
-                })
+                }),
             ];
         });
 
@@ -33,11 +55,11 @@ class ManageControllGroupMembersController
                     'id' => $user->id,
                     'name' => $user->main_character->name,
                     'character_id' => $user->main_character->character_id,
-                    'characters' => $user->characters->filter(function ($character) use ($user){
+                    'characters' => $user->characters->filter(function ($character) use ($user) {
                         return $character->character_id !== $user->main_character->character_id;
-                    })
+                    }),
                 ];
-            })
+            }),
         ]);
     }
 
@@ -45,7 +67,7 @@ class ManageControllGroupMembersController
     {
 
         $validated_data = $request->validate([
-            'selectedValues.*.id' => 'bail|integer|exists:users,id'
+            'selectedValues.*.id' => 'bail|integer|exists:users,id',
         ]);
 
         $role = Role::findById($role_id);
@@ -64,5 +86,4 @@ class ManageControllGroupMembersController
             ->with('Success', 'Control Group updated');
 
     }
-
 }
