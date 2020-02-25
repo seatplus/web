fe8e0aeb524b3727aef6a7a29b23c81a744dbbfc
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-25 14:13:24 +0000

    Apply fixes from StyleCI

--- src/Services/SsoSettings/SearchCorporationOrAlliance.php
+++ src/Services/SsoSettings/SearchCorporationOrAlliance.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Web\Services\SsoSettings;
-
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
@@ -28,10 +50,10 @@ class SearchCorporationOrAlliance
         $this->retrieve_esi_data_action = new RetrieveEsiDataAction;
     }
 
-    public function search() : string
+    public function search(): string
     {
         // if search parameter is to short esi will not respond
-        if(Str::length($this->searchParam) <3)
+        if(Str::length($this->searchParam) < 3)
             // return an empty json response string
             return collect()->toJson();
 
@@ -42,16 +64,16 @@ class SearchCorporationOrAlliance
         return $result->toJson();
     }
 
-    private function searchEsi() : array
+    private function searchEsi(): array
     {
         $container = new EsiRequestContainer([
             'method' => 'get',
             'version' => 'v2',
             'endpoint' => '/search/',
             'query_string' => [
-                'categories' => ['alliance','corporation'],
-                'search' => $this->searchParam
-            ]
+                'categories' => ['alliance', 'corporation'],
+                'search' => $this->searchParam,
+            ],
         ]);
 
         $result = $this->retrieve_esi_data_action->execute($container);
@@ -59,18 +81,17 @@ class SearchCorporationOrAlliance
         return collect($result)->flatten()->toArray();
     }
 
-    private function getNamesFromIds(array $entity_ids_that_match_sub_string) : Collection
+    private function getNamesFromIds(array $entity_ids_that_match_sub_string): Collection
     {
         $container = new EsiRequestContainer([
             'method' => 'post',
             'version' => 'v3',
             'endpoint' => '/universe/names/',
-            'request_body' => $entity_ids_that_match_sub_string
+            'request_body' => $entity_ids_that_match_sub_string,
         ]);
 
         $result = $this->retrieve_esi_data_action->execute($container);
 
         return collect($result);
     }
-
 }
