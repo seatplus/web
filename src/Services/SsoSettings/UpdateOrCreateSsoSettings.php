18e65f5da1ed38dd13ce2f3e51436be258d1a55b
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-25 12:19:21 +0000

    Apply fixes from StyleCI

--- src/Services/SsoSettings/UpdateOrCreateSsoSettings.php
+++ src/Services/SsoSettings/UpdateOrCreateSsoSettings.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Web\Services\SsoSettings;
-
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
@@ -32,7 +54,7 @@ class UpdateOrCreateSsoSettings
     {
 
         $this->request = $request;
-        $this->selected_scopes = collect(Arr::get($this->request,'selectedScopes')); //collect($this->request->input('selectedScopes'))->toJson();
+        $this->selected_scopes = collect(Arr::get($this->request, 'selectedScopes')); //collect($this->request->input('selectedScopes'))->toJson();
         $this->entity = Arr::get($this->request, 'selectedCorpOrAlliance');
         $this->entity_id = Arr::get($this->entity, 'id');
     }
@@ -45,9 +67,9 @@ class UpdateOrCreateSsoSettings
 
         SsoScopes::updateOrCreate([
             'morphable_id' => $this->entity_id,
-            'selected_scopes' => $this->selected_scopes
+            'selected_scopes' => $this->selected_scopes,
         ], [
-            'morphable_type' => $morphable_type
+            'morphable_type' => $morphable_type,
         ]);
     }
 
@@ -67,5 +89,4 @@ class UpdateOrCreateSsoSettings
     {
         (new CorporationInfoAction)->execute($this->entity_id);
     }
-
 }
