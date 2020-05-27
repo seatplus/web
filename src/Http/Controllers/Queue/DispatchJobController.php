1e506cde64834fc0224ce3ff49b1debfb4ddf29c
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-05-27 21:21:59 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Queue/DispatchJobController.php
+++ src/Http/Controllers/Queue/DispatchJobController.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Web\Http\Controllers\Queue;
-
 
 use Seatplus\Eveapi\Models\RefreshToken;
 use Seatplus\Eveapi\Services\DispatchIndividualUpdate;
@@ -18,9 +40,8 @@ class DispatchJobController extends Controller
 
         $cache_key = sprintf('%s:%s', $job['job'], $job['character_id']);
 
-        if( cache($cache_key) )
+        if(cache($cache_key))
             return redirect()->back()->with('error', 'job was already queued');
-
 
         $job_id = (new DispatchIndividualUpdate($refresh_token))->execute($job['job']);
 
@@ -28,5 +49,4 @@ class DispatchJobController extends Controller
 
         return redirect()->back()->with('success', 'job queued');
     }
-
 }
