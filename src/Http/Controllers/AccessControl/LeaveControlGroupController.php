b6e8f3a26f37c90200cbf4e9d67dd2042e10117c
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-07-20 09:23:41 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/AccessControl/LeaveControlGroupController.php
+++ src/Http/Controllers/AccessControl/LeaveControlGroupController.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Web\Http\Controllers\AccessControl;
-
 
 use Seatplus\Auth\Models\Permissions\Role;
 use Seatplus\Auth\Models\User;
@@ -19,7 +41,7 @@ class LeaveControlGroupController extends Controller
         $this->role = Role::find($role_id);
         $this->user = User::find($user_id);
 
-        if(!in_array($this->role->type, ['opt-in', 'on-request']))
+        if(! in_array($this->role->type, ['opt-in', 'on-request']))
             return abort(403, 'This action is not allowed on this access control group');
 
         $this->isActionOnYourself()
@@ -34,12 +56,12 @@ class LeaveControlGroupController extends Controller
         $this->role->removeMember($this->user);
     }
 
-    private function isSuperuserOrModerator() : bool
+    private function isSuperuserOrModerator(): bool
     {
-        return (auth()->user()->can('superuser') || $this->role->isModerator(auth()->user()));
+        return auth()->user()->can('superuser') || $this->role->isModerator(auth()->user());
     }
 
-    private function isActionOnYourself() : bool
+    private function isActionOnYourself(): bool
     {
         return auth()->user()->getAuthIdentifier() === $this->user->id;
     }
