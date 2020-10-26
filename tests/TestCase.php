a618d48dde31b5d1d647fd41d16a367bc5ad267d
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-10-26 21:49:12 +0100

    cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -114,16 +114,4 @@ abstract class TestCase extends OrchestraTestCase
         $app['config']->set('cache.prefix', 'seatplus_tests---');
     }
 
-    /**
-     * Create the test response instance from the given response.
-     *
-     * @param  \Illuminate\Http\Response $response
-     *
-     * @return \Illuminate\Foundation\Testing\TestResponse
-     */
-    /*protected function createTestResponse($response)
-    {
-        return TestResponse::fromBaseResponse($response);
-    }*/
-
 }
