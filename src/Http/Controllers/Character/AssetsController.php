882d513dea9306f9901b58c74c6bda54f415756b
Herpaderp Aldent <herpaderpaldent@users.noreply.github.com>
2020-12-28 12:46:46 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Character/AssetsController.php
+++ src/Http/Controllers/Character/AssetsController.php
@@ -30,12 +30,10 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Laravel\Horizon\Contracts\JobRepository;
 use Seatplus\Eveapi\Http\Resources\CharacterAsset as CharacterAssetResource;
-use Seatplus\Eveapi\Jobs\Assets\CharacterAssetJob;
 use Seatplus\Eveapi\Jobs\Hydrate\Character\CharacterAssetsHydrateBatch;
 use Seatplus\Eveapi\Models\Assets\CharacterAsset;
 use Seatplus\Eveapi\Models\Universe\Region;
 use Seatplus\Web\Http\Controllers\Controller;
-use Seatplus\Web\Services\DispatchUpdates\BuildDispatchableCharacterJobs;
 
 class AssetsController extends Controller
 {
@@ -72,13 +70,13 @@ class AssetsController extends Controller
         ]);
     }
 
-    private function buildDispatchTransferObject() : object
+    private function buildDispatchTransferObject(): object
     {
         return (object) [
             'manual_job' => array_search(CharacterAssetsHydrateBatch::class, config('web.jobs')),
             'permission' => config('eveapi.permissions.' . CharacterAsset::class),
             'required_scopes' => config('eveapi.scopes.character.assets'),
-            'required_corporation_role' => ''
+            'required_corporation_role' => '',
         ];
     }
 }
