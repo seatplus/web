8a32ac7da66d47743c864eae3f03049fd39d1bea
Herpaderp Aldent <felix.a.huber@gmx.net>
2021-02-02 07:55:42 +0100

    fix enlistment slideover

--- src/resources/js/Shared/Components/SlideOverComponent.vue
+++ src/resources/js/Shared/Components/SlideOverComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<!--    TODO: create vue3 portal implementation-->
     <div>
 
         <SlideOver v-model="update_open">
@@ -11,6 +12,11 @@
             <CharacterSelection :dispatch_transfer_object="dispatch_transfer_object" />
         </SlideOver>
 
+        <SlideOver v-model="create_enlistment">
+            <template v-slot:title>Create Enlistment</template>
+            <CorporationList/>
+        </SlideOver>
+
     </div>
 
 </template>
@@ -19,18 +25,21 @@
 import SlideOver from "@/Shared/Layout/SlideOver";
 import DispatchUpdate from "@/Shared/DispatchUpdate";
 import CharacterSelection from "@/Shared/Components/SlideOver/CharacterSelection";
+import CorporationList from "@/Pages/Corporation/Recruitment/CorporationList";
+
 export default {
     name: "SlideOverComponent",
-    components: {CharacterSelection, DispatchUpdate, SlideOver},
+    components: {CorporationList, CharacterSelection, DispatchUpdate, SlideOver},
     props: {
         dispatch_transfer_object: {
-            required: true,
+            required: false,
             type: Object
         },
     },
     data: () => ({
         update_open: false,
         select_character_open: false,
+        create_enlistment: false
     }),
     mounted() {
         this.$eventBus.$on('open-slideOver', (payload) => {
@@ -39,6 +48,9 @@ export default {
 
             if(payload === 'character')
                 this.select_character_open = true
+
+            if(payload === 'enlistment')
+                this.create_enlistment = true
 
         })
     }
