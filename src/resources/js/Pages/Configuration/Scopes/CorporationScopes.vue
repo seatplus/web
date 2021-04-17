4c2aecf66daf79c4b5f2743eedb57997c9ba6edb
Felix Huber <felix.a.huber@gmx.net>
2021-04-17 22:25:21 +0200

    Fix scope setting * add toggles * update RadioListWithDescription.vue

--- src/resources/js/Pages/Configuration/Scopes/CorporationScopes.vue
+++ src/resources/js/Pages/Configuration/Scopes/CorporationScopes.vue
@@ -1,60 +1,90 @@
 <template>
-    <div>
-        <h3 class="text-lg font-medium leading-6 text-gray-900">Corporation Scopes</h3>
-        <div v-for="entity in flavours">
-            <label  class="inline-flex items-center">
-                <input
-                    type="checkbox"
-                    :value="entity.value"
-                    v-model="selected"
-                    class="form-checkbox"
-                    @change="$emit('corporation-scopes', selected)"
-                >
-                <span class="ml-2">{{entity.text}}</span>
-            </label>
-        </div>
+  <div>
+    <h3 class="text-lg font-medium leading-6 text-gray-900">
+      Corporation Scopes
+    </h3>
+    <div class="space-y-1">
+      <ScopeToggle
+        v-for="flavour in flavours"
+        :key="`${flavour.text}:${scopesAsString}`"
+        v-model:selected="selected"
+        :scope="flavour"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CorporationScopes",
-        props: {
-            scopes: {
-                type: Object,
-                required: true
-            },
-            selectedScopes: {
-                type    : Array,
-                required: false,
-                default : []
-            }
+import ScopeToggle from "./ScopeToggle";
+import {computed, ref, watch} from "vue";
+export default {
+    name: "CorporationScopes",
+    components: {ScopeToggle},
+    props: {
+        scopes: {
+            type: Object,
+            required: true
         },
-        data() {
-            return {
-                selected: []
-            }
-        },
-        computed: {
-            flavours: function () {
-                return _.map(this.scopes, (value, prop) => ({
-                    text: _.capitalize(prop),
-                    value: _.toString(value)
-                }));
-            },
-        },
-        mounted() {
-            /*TODO this.$eventBus.$on('role-removed', () => {
-                this.selected = []
-                this.$emit('corporation-scopes', this.selected)
-            })*/
-        },
-        watch: {
-            selectedScopes() {
-                this.selected = this.selectedScopes
-            }
+        selectedScopes: {
+            type    : Array,
+            required: false,
+            default: () => []
         }
-    }
+    },
+    emits: ['update:selectedScopes'],
+    setup(props, { emit }) {
+
+        const selected = ref(filter())
+        const scopes= ref(_.toArray(props.scopes))
+
+        function filter() {
+
+            if(!props.selectedScopes.includes('esi-characters.read_corporation_roles.v1')) {
+
+                emit('update:selectedScopes', _.pullAll(props.selectedScopes, _.flatten(_.toArray(props.scopes))))
+            }
+
+            return props.selectedScopes
+        }
+
+        const hasCorporationScopesSelected = computed( () =>  {
+            return _.chain(scopes.value)
+                .toArray()
+                .flatten()
+                .intersection(selected.value)
+                .value().length > 0
+        })
+
+        watch(selected, (newValue) => {
+
+            // add corporation division if wallet or assets is selected
+            if(_.intersection(['esi-wallet.read_corporation_wallets.v1', 'esi-assets.read_corporation_assets.v1'], newValue).length > 0)
+                newValue.push('esi-corporations.read_divisions.v1')
+
+            // if any corporation scope is selected and role is not yet selected, push role scope
+            if(hasCorporationScopesSelected.value && !newValue.includes('esi-characters.read_corporation_roles.v1'))
+                newValue.push('esi-characters.read_corporation_roles.v1')
+
+            emit('update:selectedScopes', _.uniq(newValue))
+        })
+
+
+        return {
+            selected
+        }
+    },
+    computed: {
+        flavours: function () {
+            return _.map(this.scopes, (value, prop) => ({
+                text: _.capitalize(prop),
+                value: value
+            }));
+        },
+        scopesAsString() {
+            return _.toString(this.selectedScopes)
+        },
+    },
+}
 </script>
 
 <style scoped>
