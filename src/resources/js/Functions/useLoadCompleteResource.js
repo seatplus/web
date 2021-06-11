755ef40f430e47d2e1222846958fc281b80c04bc
Felix Huber <felix.a.huber@gmx.net>
2021-06-11 22:24:15 +0200

    reduce complexity

--- src/resources/js/Functions/useLoadCompleteResource.js
+++ src/resources/js/Functions/useLoadCompleteResource.js
@@ -6,25 +6,12 @@ export function useLoadCompleteResource(routeName, params) {
 
     const url = ref(route(routeName,useHydrateQueryParameters(params)))
     const results = ref([])
-    const page = ref(1)
-
-    const isInitialRequestLoading = ref(true)
-    const isLoading = ref(false)
-    const isComplete = ref(false)
 
     const fetchData = async () => {
 
-        if(isLoading.value || isComplete.value)
-            return
-
-        isLoading.value = true
         let last_page = 1
 
-        await axios.get(url.value, {
-            params: {
-                page: page.value,
-            },
-        })
+        await axios.get(url.value, { params: { page: 1 }})
             .then(response => {
 
                 last_page = response.data.last_page
@@ -34,8 +21,6 @@ export function useLoadCompleteResource(routeName, params) {
                 }
             })
             .catch(error => console.log(error))
-            .finally(isInitialRequestLoading.value = false)
-
 
         const axiosrequests = []
 
@@ -43,23 +28,17 @@ export function useLoadCompleteResource(routeName, params) {
             axiosrequests.push(axios.get(url.value, {params: {page: i}}))
         }
 
-        await axios.all(axiosrequests).then(response => response.forEach(element => results.value.push(...element.data.data)))
-            .finally(() => {
-                isLoading.value = false
-                isComplete.value = true
-            })
+        await axios.all(axiosrequests)
+            .then(response => response.forEach(element => results.value.push(...element.data.data)))
+            .catch(error => console.log(error))
 
     }
 
     onBeforeMount(async () => {
         await fetchData()
-        isInitialRequestLoading.value = false
     })
 
     return {
         results,
-        isLoading,
-        isComplete,
-        isInitialRequestLoading
     }
 }
