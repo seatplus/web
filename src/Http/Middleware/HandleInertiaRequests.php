5ff0d926d8c16d07c621600a4c259f50e1f992a6
Herpaderp Aldent <herpaderpaldent@users.noreply.github.com>
2021-09-27 18:10:22 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/HandleInertiaRequests.php
+++ src/Http/Middleware/HandleInertiaRequests.php
@@ -45,6 +45,7 @@ class HandleInertiaRequests extends Middleware
      * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
+     *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
@@ -57,6 +58,7 @@ class HandleInertiaRequests extends Middleware
      * Defines the props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
+     *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
