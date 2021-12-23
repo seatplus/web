d493379ab76d0918e372e443af6f62daa951177d
Herpaderp Aldent <herpaderpaldent@users.noreply.github.com>
2021-12-23 14:49:23 +0000

    Apply fixes from StyleCI

--- src/Http/Actions/Corporation/Recruitment/UpdateWatchlistAction.php
+++ src/Http/Actions/Corporation/Recruitment/UpdateWatchlistAction.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020, 2021 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace Seatplus\Web\Http\Actions\Corporation\Recruitment;
 
 use Illuminate\Support\Arr;
@@ -20,19 +44,17 @@ class UpdateWatchlistAction
 
     public function execute(int $corporation_id, array $validated_data)
     {
-
         $this->enlistment = Enlistment::find($corporation_id);
         $this->validatedData = $validated_data;
 
         $this->handleSystems();
         $this->handleRegions();
         $this->handleItems();
-
     }
 
     private function handleSystems()
     {
-        if(!Arr::has($this->validatedData, 'systems')) {
+        if (! Arr::has($this->validatedData, 'systems')) {
             // return early if systems is not part of the validated data, f.e. when updating items only
             return;
         }
@@ -47,7 +69,7 @@ class UpdateWatchlistAction
 
     private function handleRegions()
     {
-        if(!Arr::has($this->validatedData, 'regions')) {
+        if (! Arr::has($this->validatedData, 'regions')) {
             // return early if systems is not part of the validated data, f.e. when updating items only
             return;
         }
@@ -62,25 +84,25 @@ class UpdateWatchlistAction
 
     private function handleItems()
     {
-        if(!Arr::has($this->validatedData, 'items')) {
+        if (! Arr::has($this->validatedData, 'items')) {
             // return early if systems is not part of the validated data, f.e. when updating regions
             return;
         }
 
         collect(data_get($this->validatedData, 'items', []))
-            ->groupBy('watchable_type')->pipe(function(Collection $collection) {
+            ->groupBy('watchable_type')->pipe(function (Collection $collection) {
                 foreach ([Type::class, Group::class, Category::class] as $key) {
-                    if (!$collection->has($key)) {
+                    if (! $collection->has($key)) {
                         $collection = $collection->mergeRecursive([$key => []]);
                     }
                 }
+
                 return $collection;
             })
-            ->each(fn($items, $category) => match($category) {
+            ->each(fn ($items, $category) => match ($category) {
                 Type::class => $this->enlistment->types()->sync(data_get($items, '*.watchable_id')),
                 Group::class => $this->enlistment->groups()->sync(data_get($items, '*.watchable_id')),
                 Category::class => $this->enlistment->categories()->sync(data_get($items, '*.watchable_id')),
             });
-
     }
 }
--- src/Http/Actions/Corporation/Recruitment/WatchedArrayAction.php
+++ src/Http/Actions/Corporation/Recruitment/WatchedArrayAction.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020, 2021 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace Seatplus\Web\Http\Actions\Corporation\Recruitment;
 
 use Seatplus\Eveapi\Models\Universe\Category;
@@ -13,7 +37,7 @@ class WatchedArrayAction
 
     private Enlistment $enlistment;
 
-    public function execute(int $corporation_id) : array
+    public function execute(int $corporation_id): array
     {
         /** @noinspection PhpFieldAssignmentTypeMismatchInspection */
         $this->enlistment = Enlistment::with(
@@ -46,25 +70,25 @@ class WatchedArrayAction
     private function handleItems()
     {
         $types = $this->enlistment->types->map(fn ($type) => [
-                'id' => intval(1 . $type->type_id),
-                'name' => $type->name . "(type)",
-                'watchable_id' => $type->type_id,
-                'watchable_type' => Type::class
-            ]) ?? [];
+            'id' => intval(1 . $type->type_id),
+            'name' => $type->name . '(type)',
+            'watchable_id' => $type->type_id,
+            'watchable_type' => Type::class,
+        ]) ?? [];
 
         $groups = $this->enlistment->groups->map(fn ($group) => [
-                'id' => intval(1 . $group->group_id),
-                'name' => $group->name . "(group)",
-                'watchable_id' => $group->group_id,
-                'watchable_type' => Group::class
-            ]) ?? [];
+            'id' => intval(1 . $group->group_id),
+            'name' => $group->name . '(group)',
+            'watchable_id' => $group->group_id,
+            'watchable_type' => Group::class,
+        ]) ?? [];
 
         $categories = $this->enlistment->categories->map(fn ($category) => [
-                'id' => intval(1 . $category->category_id),
-                'name' => $category->name . "(category)",
-                'watchable_id' => $category->category_id,
-                'watchable_type' => Category::class
-            ]) ?? [];
+            'id' => intval(1 . $category->category_id),
+            'name' => $category->name . '(category)',
+            'watchable_id' => $category->category_id,
+            'watchable_type' => Category::class,
+        ]) ?? [];
 
         data_set($this->watched, 'items', [...$types, ...$groups, ...$categories]);
     }
@@ -72,10 +96,10 @@ class WatchedArrayAction
     private function handleRegions()
     {
         $entries = $this->enlistment->regions->map(function ($region) {
-                $region->id = $region->region_id;
+            $region->id = $region->region_id;
 
-                return $region;
-            }) ?? [];
+            return $region;
+        }) ?? [];
 
         data_set($this->watched, 'regions', $entries);
     }
