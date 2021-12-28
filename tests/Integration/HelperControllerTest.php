c9aa176bc88cf8dbf7b81292446f659d59c56c07
Felix Huber <felix.a.huber@gmx.net>
2021-12-28 14:07:37 +0100

    Cache image variants

--- tests/Integration/HelperControllerTest.php
+++ tests/Integration/HelperControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Illuminate\Support\Facades\Cache;
 use Seatplus\Eveapi\Containers\EsiRequestContainer;
 use Seatplus\Eveapi\Models\Universe\Category;
 use Seatplus\Eveapi\Models\Universe\Group;
@@ -195,17 +196,35 @@ test('one can search existing region', function () {
     expect($result->original)->toHaveCount(1);
 });
 
-test('on can get resource variants', function () {
+test('one can get resource variants via http and cache', function () {
     Http::fake();
 
+    $resource_type = 'types';
+    $resource_id = 587;
+    $url = "https://images.evetech.net/${resource_type}/${resource_id}";
     $expected_response = ["render", "icon"];
 
     Http::shouldReceive('get->json')->once()->andReturn(json_encode($expected_response));
+    Cache::shouldReceive('get')
+        ->twice()
+        ->with($url)
+        ->andReturns(null, $expected_response);
+    Cache::shouldReceive('put')->once();
 
+    // first time miss cache
     $result = test()->actingAs(test()->test_user)
         ->get(route('get.resource.variants', [
-            'resource_type' => 'types',
-            'resource_id' => 587
+            'resource_type' => $resource_type,
+            'resource_id' => $resource_id
+        ]))
+        ->assertOk()
+        ->assertJson($expected_response);
+
+    // second time hit cache
+    $result = test()->actingAs(test()->test_user)
+        ->get(route('get.resource.variants', [
+            'resource_type' => $resource_type,
+            'resource_id' => $resource_id
         ]))
         ->assertOk()
         ->assertJson($expected_response);
