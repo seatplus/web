7e0642ea47a28586f5f8e0c4c9b51a1770ff683e
Felix Huber <felix.a.huber@gmx.net>
2022-01-21 15:16:12 +0100

    fix alignment of main_character * add flex * move to common table component

---
+++ src/resources/js/Pages/Corporation/Recruitment/ApplicationsTable/ApplicationsTable.vue
@@ -0,0 +1,88 @@
+<template>
+  <StickyHeaderTable :header-titles="headerTitles">
+    <template #default="{ countColumns, columns }">
+      <StickyHeaderTableRow
+        v-for="applicant in applications"
+        :key="applicant"
+        :number-columns="countColumns"
+      >
+        <StickyHeaderCell
+          :cell="columns[0]"
+          class="flex truncate"
+        >
+          <div class="flex-shrink self-center">
+            <EntityByIdBlock
+              :id="applicant.main_character.character_id"
+              class="truncate"
+              :image-size="10"
+            />
+          </div>
+        </StickyHeaderCell>
+
+        <StickyHeaderCell
+          :cell="columns[1]"
+          class="self-center"
+        >
+          <div class="flex gap-x-2 flex-wrap">
+            <CharacterComplianceElement
+              v-for="character in applicant.characters"
+              :key="character"
+              :character="character"
+            />
+          </div>
+        </StickyHeaderCell>
+
+        <StickyHeaderCell
+          :cell="columns[2]"
+          class="self-center"
+        >
+          <div class="flex justify-end">
+            <Button
+              button-size="xs"
+              :href="$route('get.application', applicant.application_id)"
+            >
+              Review
+            </Button>
+          </div>
+        </stickyheadercell>
+      </StickyHeaderTableRow>
+    </template>
+  </StickyHeaderTable>
+</template>
+
+<script>
+import StickyHeaderTable from "@/Shared/Layout/Table/StickyHeaderTable";
+import StickyHeaderTableRow from "@/Shared/Layout/Table/StickyHeaderTableRow";
+import StickyHeaderCell from "@/Shared/Layout/Table/StickyHeaderCell";
+import EntityByIdBlock from "@/Shared/Layout/Eve/EntityByIdBlock";
+import CharacterComplianceElement from "@/Pages/Corporation/MemberCompliance/CharacterComplianceElement";
+import Button from "@/Shared/Layout/Button";
+
+let headerTitles = [
+    {title: 'Main Character', columnSpan: 3},
+    {title: 'Characters', columnSpan: 7},
+    {title: 'Review', columnSpan: 2, srOnly: true},
+];
+
+export default {
+    name: "ApplicationsTable",
+    components: {
+        Button,
+        CharacterComplianceElement, EntityByIdBlock, StickyHeaderCell, StickyHeaderTableRow, StickyHeaderTable},
+    props: {
+        applications: {
+            required: true,
+            type: Array
+        }
+    },
+    setup() {
+        return {
+            headerTitles
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/resources/js/Pages/Corporation/Recruitment/ApplicationsTable/PendingTable.vue
+++ src/resources/js/Pages/Corporation/Recruitment/ApplicationsTable/PendingTable.vue
@@ -4,77 +4,21 @@
     :params="{corporation_id: corporationId}"
     @result="(results) => raw_pending = results"
   >
-    <StickyHeaderTable :header-titles="headerTitles">
-      <template #default="{ countColumns, columns }">
-        <StickyHeaderTableRow
-          v-for="applicant in pending"
-          :key="applicant"
-          :number-columns="countColumns"
-        >
-          <StickyHeaderCell :cell="columns[0]">
-            <div class="flex-shrink self-center">
-              <EntityByIdBlock
-                :id="applicant.main_character.character_id"
-                class="truncate"
-                :image-size="10"
-              />
-            </div>
-          </StickyHeaderCell>
-
-          <StickyHeaderCell
-            :cell="columns[1]"
-            class="self-center"
-          >
-            <div class="flex gap-x-2 flex-wrap">
-              <CharacterComplianceElement
-                v-for="character in applicant.characters"
-                :key="character"
-                :character="character"
-              />
-            </div>
-          </StickyHeaderCell>
-
-          <StickyHeaderCell
-            :cell="columns[2]"
-            class="self-center"
-          >
-            <div class="flex justify-end">
-              <Button
-                button-size="xs"
-                :href="$route('get.application', applicant.application_id)"
-              >
-                Review
-              </Button>
-            </div>
-          </stickyheadercell>
-        </StickyHeaderTableRow>
-      </template>
-    </StickyHeaderTable>
+    <ApplicationsTable :applications="pending" />
   </InfiniteLoadingHelper>
 </template>
 
 <script>
 import InfiniteLoadingHelper from "@/Shared/InfiniteLoadingHelper";
-import StickyHeaderTable from "@/Shared/Layout/Table/StickyHeaderTable";
-import StickyHeaderTableRow from "@/Shared/Layout/Table/StickyHeaderTableRow";
-import EntityByIdBlock from "@/Shared/Layout/Eve/EntityByIdBlock";
-import StickyHeaderCell from "@/Shared/Layout/Table/StickyHeaderCell";
-import CharacterComplianceElement from "@/Pages/Corporation/MemberCompliance/CharacterComplianceElement";
 import {computed, ref} from "vue";
-import Button from "@/Shared/Layout/Button";
-
-let headerTitles = [
-    {title: 'Main Character', columnSpan: 3},
-    {title: 'Characters', columnSpan: 7},
-    {title: 'Review', columnSpan: 2, srOnly: true},
-];
+import ApplicationsTable from "@/Pages/Corporation/Recruitment/ApplicationsTable/ApplicationsTable";
 
 export default {
     name: "PendingTable",
     components: {
-        Button,
-        CharacterComplianceElement,
-        StickyHeaderCell, EntityByIdBlock, StickyHeaderTableRow, StickyHeaderTable, InfiniteLoadingHelper},
+        ApplicationsTable,
+         InfiniteLoadingHelper,
+    },
     props: {
         stepCount: {
             required: true,
@@ -93,8 +37,7 @@ export default {
 
         return {
             raw_pending,
-            pending,
-            headerTitles
+            pending
         }
     }
 }
