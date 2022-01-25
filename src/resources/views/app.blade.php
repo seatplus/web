d63d9d6a7d772b33b59c7e11fcdb2d07eb197e54
Herpaderp Aldent <felix.a.huber@gmx.net>
2022-01-25 08:05:02 +0100

    add icon to blade

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head id="head">
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-  <script src="{{ mix('/js/app.js') }}" defer></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" href="/css/favicon.ico">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
     @translations
 
