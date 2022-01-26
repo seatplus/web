04542c0b109f4efe646d83c01d4faf3bfab96833
Felix Huber <felix.a.huber@gmx.net>
2022-01-26 15:00:14 +0100

    use svg

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <link rel="icon" href="/css/favicon.ico">
+    <link rel="icon" href="/img/seat_plus_logo.svg">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
