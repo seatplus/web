0281b8f11dceca4b4a2307d6e7099844d3c843b8
Felix Huber <felix.a.huber@gmx.net>
2023-02-23 22:07:47 +0100

    wip

---
+++ resources/js/Pages/Configuration/Performance/Overview.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="space-y-3">
+    <teleport to="#head">
+      <title>{{ title(pageTitle) }}</title>
+    </teleport>
+
+    <PageHeader>
+      {{ pageTitle }}
+    </PageHeader>
+
+    <CompleteInertiaLoaderHelper v-slot="{data}">
+      {{ getDate(data) }}
+    </CompleteInertiaLoaderHelper>
+
+
+  </div>
+</template>
+
+<script setup>
+
+import PageHeader from "@/Shared/Layout/PageHeader.vue";
+import CompleteInertiaLoaderHelper from "@/Shared/CompleteInertiaLoaderHelper.vue";
+import { map } from 'lodash'
+import dayjs from "dayjs";
+
+const pageTitle = 'Server Performance'
+
+function getDate(data) {
+
+  // mpa each data item with a new day property
+
+  return  map(data, function (data) {
+    data.day = dayjs(data.finished_at).format('YYYY-MM-DD')
+    return data
+  })
+}
+
+</script>
---
+++ resources/js/Shared/CompleteInertiaLoaderHelper.vue
@@ -0,0 +1,98 @@
+<template>
+  <div v-if="!is_complete"
+      class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
+    <svg
+        class="animate-spin mx-auto h-12 w-12 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+    >
+      <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+      />
+      <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+    <span class="mt-2 block text-sm font-medium text-gray-900">
+          loading resource
+        </span>
+  </div>
+  <slot v-if="data.length > 0" :data="data" />
+</template>
+
+<script setup>
+
+import {onMounted, onUnmounted, ref} from "vue";
+import { router } from '@inertiajs/vue3'
+
+const data = ref([])
+const is_complete = ref(false)
+
+const props = defineProps({
+  href: {
+    type: String,
+    // set default value to be current route
+    default: () => window.location.href
+  },
+  method: {
+    type: String,
+    default: 'GET'
+  },
+  only: {
+    type: String,
+    default: 'data'
+  }
+})
+
+const initialUrl = window.location.href
+const next_page_url = ref(initialUrl)
+const cancelToken = ref(null)
+
+function loadMore() {
+  if (is_complete.value) {
+    return
+  }
+
+  // return if next page url is not set
+  if (!next_page_url.value) {
+    return
+  }
+
+  router.visit(props.href, {
+    method: props.method,
+    preserveState: true,
+    preserveScroll: true,
+    only: [props.only],
+    onCancelToken: (token) => {
+      cancelToken.value = token
+    },
+    onSuccess: (page) => {
+      data.value = data.value.concat(page.props[props.only].data)
+      next_page_url.value = page.props[props.only].next_page_url
+      is_complete.value = !next_page_url.value
+
+      loadMore()
+    }
+  })
+}
+
+onMounted(() => {
+  loadMore()
+})
+
+onUnmounted(() => {
+  if (cancelToken.value) {
+    cancelToken.value.cancel()
+  }
+})
+
+</script>
