39d5438a281f99a94e2a21a360af7a74b8954518
herpaderpaldent <herpaderpaldent@users.noreply.github.com>
2023-02-23 21:08:22 +0000

    Fix styling

--- src/Http/Controllers/Configuration/PerformanceController.php
+++ src/Http/Controllers/Configuration/PerformanceController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Seatplus\Web\Http\Controllers\Configuration;
+
 use Inertia\Inertia;
 use Seatplus\Eveapi\Models\BatchStatistic;
 use Seatplus\Web\Http\Controllers\Controller;
@@ -10,7 +11,7 @@ class PerformanceController extends Controller
     public function index()
     {
         return inertia('Configuration/Performance/Overview', [
-            'data' => Inertia::lazy(fn() => BatchStatistic::paginate(50)),
+            'data' => Inertia::lazy(fn () => BatchStatistic::paginate(50)),
         ]);
     }
 }
