f5d81910cfdd939c7387a54368717c7f14cb51de
Felix Huber <felix.a.huber@gmx.net>
2023-03-17 22:16:05 +0100

    replace vue-chart-3 implementation

--- resources/js/Shared/Components/Wallet/Journal/WalletJournalBalanceChart.vue
+++ resources/js/Shared/Components/Wallet/Journal/WalletJournalBalanceChart.vue
@@ -12,104 +12,88 @@
       </div>
     </template>
     <div class="relative max-h-48 overflow-y-auto">
-      <LineChart
-        v-if="results.length > 0"
-        class="p-2"
-        v-bind="lineChartProps"
-      />
+      <Line :data="chartData" :options="chartOptions" />
     </div>
   </CardWithHeader>
 </template>
 
-<script>
+<script setup>
 import CardWithHeader from "@/Shared/Layout/Cards/CardWithHeader.vue";
 import EntityByIdBlock from "@/Shared/Layout/Eve/EntityByIdBlock.vue";
 import {useLoadCompleteResource} from "@/Functions/useLoadCompleteResource";
-import {LineChart, useLineChart } from "vue-chart-3";
+import {Line} from 'vue-chartjs'
 import {computed} from "vue";
-import { Chart, registerables } from 'chart.js';
+import {CategoryScale, Chart, Colors, LinearScale, LineElement, PointElement, Tooltip} from 'chart.js';
 
-Chart.register(...registerables);
+Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Colors
+);
 
-export default {
-    name: "WalletJournalBalanceChart",
-    components: {
-        LineChart,
-        EntityByIdBlock,
-        CardWithHeader
-    },
-    props: {
-        id: {
-            required: true,
-            type: Number
-        },
-        division: {
-            required: false,
-            type: Object,
-            default: () => {}
-        }
-    },
-    setup(props) {
+const props = defineProps({
+  id: {
+    required: true,
+    type: Number
+  },
+  division: {
+    required: false,
+    type: Object,
+    default: () => {}
+  }
+})
 
-        let routeName = props.division? 'corporation.balance' : 'character.balance'
-        let routeParameters = props.division ? {
-            corporation_id: props.id,
-            division_id: props.division.division_id
-        } : {
-            character_id: props.id
-        }
-
-        const chartOptions = {
-            responsive: true,
-                maintainAspectRatio: false,
-                legend: {
-                display: false
-            },
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    distribution: 'linear'
-                }],
-                    yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        callback: function(value) {
-                            return 'ISK ' + value.toLocaleString();
-                        }
-                    }
-                }],
-            }
-        }
-
-        const {results} = useLoadCompleteResource(routeName, routeParameters)
-
-        const chartData = computed(() => {
-            return {
-                labels: _.map(results.value, (result) => result.x),
-                datasets: [{
-                    label: 'ISK',
-                    data: _.map(results.value, (result) => result.y),
-                    borderWidth: 3,
-                    fill: false,
-                    borderColor: '#4f46e5'
-                }]
-            }
-        })
-
-        const {lineChartProps, lineChartRef} = useLineChart({
-            chartData, chartOptions, style: {height: '12rem', position: 'relative'}
-        })
-
-
-        return {
-            results,
-            lineChartProps,
-            lineChartRef
-        }
-    },
+let routeName = props.division? 'corporation.balance' : 'character.balance'
+let routeParameters = props.division ? {
+  corporation_id: props.id,
+  division_id: props.division.division_id
+} : {
+  character_id: props.id
 }
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  animation: false,
+  legend: {
+    display: false
+  },
+  scales: {
+    x: {
+      ticks: {
+        source: 'auto',
+        autoSkip: true,
+      }
+    },
+    y: {
+      ticks: {
+        beginAtZero: true,
+        callback: function(value) {
+          return 'ISK ' + value.toLocaleString();
+        }
+      }
+    },
+  }
+}
+
+const {results} = useLoadCompleteResource(routeName, routeParameters)
+
+const chartData = computed(() => {
+  return {
+    labels: _.map(results.value, (result) => result.x),
+    datasets: [{
+      label: 'ISK',
+      data: _.map(results.value, (result) => result.y),
+      borderWidth: 3,
+      fill: false,
+      borderColor: '#4f46e5'
+    }]
+  }
+})
+
 </script>
 
-<style scoped>
 
-</style>
