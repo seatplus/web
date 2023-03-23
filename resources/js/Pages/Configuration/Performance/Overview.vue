42d7197a4f7eb8b47a2d36bb7113b352e049ec26
Herpaderp Aldent <felix.a.huber@gmx.net>
2023-03-23 23:33:06 +0100

    Feature/batch statistics (#1351)
    * Upgrade to inertia v1
    
    * Add performance view
    
    * fix endpoint version
    
    * remove old method
    
    * move to composition api
    
    * introduce default value for activeSidebarElement
    
    * replace vue-chart-3 implementation
    
    * Add performance view
    
    * fix endpoint version
    
    * remove old method
    
    * move to composition api
    
    * introduce default value for activeSidebarElement
    
    * replace vue-chart-3 implementation
    
    * exclude non finished batches
    
    * remove spatie/dto
    
    ---------
    
    Co-authored-by: herpaderpaldent <herpaderpaldent@users.noreply.github.com>

---
+++ resources/js/Pages/Configuration/Performance/Overview.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="space-y-3">
+    <teleport to="#head">
+      <title>{{ title(pageTitle) }}</title>
+    </teleport>
+
+    <PageHeader>
+      {{ pageTitle }}
+    </PageHeader>
+
+    <CompleteInertiaLoaderHelper v-slot="{data}">
+      <Card>
+        <Line :data="getDate(data)" :options="options" />
+      </Card>
+
+    </CompleteInertiaLoaderHelper>
+
+
+  </div>
+</template>
+
+<script setup>
+
+import PageHeader from "@/Shared/Layout/PageHeader.vue";
+import CompleteInertiaLoaderHelper from "@/Shared/CompleteInertiaLoaderHelper.vue";
+import {groupBy, map} from 'lodash'
+import {Line} from 'vue-chartjs'
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  TimeScale,
+  Tooltip,
+  Colors
+} from "chart.js";
+import Card from "@/Shared/Layout/Cards/Card.vue";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+const pageTitle = 'Server Performance'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Legend,
+    TimeScale,
+    Tooltip,
+    Colors
+);
+
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+
+function getDate(data) {
+
+  // sort data by finished_at
+  data.sort((a, b) => {
+    return new Date(a.finished_at) - new Date(b.finished_at)
+  })
+
+  const datasets = []
+
+  // group data by queue_balancing_configuration
+  const grouped = groupBy(data, 'queue_balancing_configuration')
+
+  // loop through each group
+  for (const [key, value] of Object.entries(grouped)) {
+
+    // map each data item with a new day property
+    const mapped = map(value, function (data) {
+
+      return{
+        x: data.finished_at,
+        y: data.duration
+      }
+    })
+
+    datasets.push({
+      label: key,
+      data: mapped,
+    })
+  }
+
+  const labels = map(data, function (data) {
+    return data.finished_at
+  })
+
+  return {
+    datasets: datasets,
+    labels: labels
+  }
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: true,
+
+  // Turn off animations and data parsing for performance
+  animation: false,
+
+  plugins: {
+    decimation: {
+      enabled: true,
+      algorithm: 'lttb',
+    },
+    tooltip: {
+      callbacks: {
+        label: function(context) {
+          return context.dataset.label.substring(0,42) + '...: ' + dayjs.duration(context.parsed.y, 's').humanize();
+        }
+      }
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        source: 'auto',
+        // Disabled rotation for performance
+        maxRotation: 0,
+        autoSkip: true,
+        display: false,
+      }
+    },
+    y: {
+      ticks: {
+        callback: function(value) {
+          return dayjs.duration(value, 's').humanize()
+        }
+      }
+    }
+  }
+}
+
+
+
+</script>
---
+++ src/Http/Controllers/Configuration/PerformanceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Seatplus\Web\Http\Controllers\Configuration;
+
+use Inertia\Inertia;
+use Seatplus\Eveapi\Models\BatchStatistic;
+use Seatplus\Web\Http\Controllers\Controller;
+
+class PerformanceController extends Controller
+{
+    public function index()
+    {
+        return inertia('Configuration/Performance/Overview', [
+            'data' => Inertia::lazy(
+                fn () => BatchStatistic::query()
+                ->whereNotNull('finished_at')
+                ->orderBy('finished_at', 'desc')
+                ->paginate(50)
+            ),
+        ]);
+    }
+}
