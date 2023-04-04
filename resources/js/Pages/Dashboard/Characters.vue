11c19475472523e5240d2d76226ea1091ad4dd28
Felix Huber <felix.a.huber@gmx.net>
2023-04-04 21:19:39 +0200

    don't show empty enlistments
    - fix space and padding

--- resources/js/Pages/Dashboard/Characters.vue
+++ resources/js/Pages/Dashboard/Characters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-5 border-b border-gray-200 space-y-2">
+  <div class="space-y-2">
     <h3 class="text-lg leading-6 font-medium text-gray-900">
       Characters
     </h3>
@@ -65,12 +65,6 @@
           </svg>
           Add characters
         </a>
-        <!--                <div class="w-full flex items-center justify-center p-6 space-x-6">
-                            <svg class="max-h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-                            </svg>
-                            <div>Test</div>
-                        </div>-->
       </li>
     </ul>
   </div>
