037e160502988b156c8912e887cd7a2566b0a438
Herpaderp Aldent <felix.a.huber@gmx.net>
2026-04-27 17:27:06 +0200

    test(routes): update RouteTest to allow access control routes

--- tests/Integration/RouteTest.php
+++ tests/Integration/RouteTest.php
@@ -8,20 +8,10 @@ it('protects configurations routes', function () {
         ->assertForbidden();
 });
 
-it('protects access control routes', function () {
-    $response = test()->actingAs(test()->test_user)
-        ->get(route('acl.groups'))
-        ->assertForbidden();
-});
-
-it('allows superuser on protected access control routes', function () {
-    $permission = Permission::findOrCreate('superuser');
-
-    test()->test_user->givePermissionTo($permission);
-
-    // now re-register all the roles and permissions
-
+it('does not protect access control routes', function () {
     $response = test()->actingAs(test()->test_user)
         ->get(route('acl.groups'))
         ->assertOk();
 });
+
+
